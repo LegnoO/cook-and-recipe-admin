89ab586fd6efba22d373b730ee364d929e1019b4
legno <meogaphieu12@gmail.com>
2024-08-23 03:04:03 +0700

    remove secret key

--- src/main.tsx
+++ src/main.tsx
@@ -16,15 +16,6 @@ import router from "./routes";
 import "./styles/globals.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 
-const expectedSecretKey = "12f84f38-f45e-4d0d-b37a-9db6374cbc17";
-const actualSecretKey = import.meta.env.VITE_SECRET_KEY;
-
-if (actualSecretKey !== expectedSecretKey) {
-  const messages = "Invalid Secret Key!";
-  alert(messages);
-  throw Error(messages);
-}
-
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
