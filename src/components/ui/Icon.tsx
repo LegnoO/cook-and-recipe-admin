5a2017ca0cc8836b1f0715de41f349048070d13a
legno <meogaphieu12@gmail.com>
2024-09-01 16:16:07 +0700

    save for fix

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -1,7 +1,14 @@
 import { Icon, IconProps } from "@iconify/react";
 
 const IconifyIcon = ({ icon, ...rest }: IconProps) => {
-  return <Icon style={{height:"22px",width:"22px"}} icon={icon} fontSize="1.375rem" {...rest} />;
+  return (
+    <Icon
+      style={{ height: "22px", width: "22px" }}
+      icon={icon}
+      fontSize="1.375rem"
+      {...rest}
+    />
+  );
 };
 
 export default IconifyIcon;
